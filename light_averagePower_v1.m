7f62b348f7a91b2d8f864a6c96a0e733ae3f8a9b
Thomas Andrillon <thomas.andrillon@gmail.com>
2020-04-17 14:28:16 +1000

    Add cond for av power

--- light_averagePower_v1.m
+++ light_averagePower_v1.m
@@ -19,42 +19,56 @@ for nS=1:length(List_Subj)
     
     %%% load data
     File_Name = List_Subj(nS).name;
-%     if strcmp(File_Name,'TFe_ft_DLT018.mat')
-%         continue;
-%     end
-    fprintf('... processing %s (%g/%g)\n',File_Name,nS,length(List_Subj))
+    %     if strcmp(File_Name,'TFe_ft_DLT018.mat')
+    %         continue;
+    %     end
+    fprintf('... processing %s (%g/%g)',File_Name,nS,length(List_Subj))
     File_Path = List_Subj(nS).folder;
     load([data_path filesep File_Name]);
     if nS==1
         av_logPower=nan([length(List_Subj) size(TFRhann.powspctrm,1) size(TFRhann.powspctrm,2) size(TFRhann.powspctrm,3)]);
     end
     av_logPower(nS,:,:,:)=log(squeeze(mean(TFRhann.powspctrm,4)));
-%     TFRhann.powspctrm_norm=10*log(TFRhann.powspctrm./repmat(mean(TFRhann.powspctrm(1,:,:,:),4),[size(TFRhann.powspctrm,1) 1 1 size(TFRhann.powspctrm,4)]));
+    %     TFRhann.powspctrm_norm=10*log(TFRhann.powspctrm./repmat(mean(TFRhann.powspctrm(1,:,:,:),4),[size(TFRhann.powspctrm,1) 1 1 size(TFRhann.powspctrm,4)]));
+    
+    %%% extract info
+    bound{1}=findstr(File_Name,'_');
+    bound{2}=findstr(File_Name,'.');
+    CodeSubj=File_Name(bound{1}(end)+1:bound{2}(1)-1);
+    CondSubj(nS)=SubInfo.Condition(find(~cellfun(@isempty,regexpi(SubInfo.PT_Code,CodeSubj))));
+    fprintf('... condition %s\n',CondSubj(nS))
+    
 end
 
 %%
+Conds={'E','D'};
 Colors={[100,100,100
-127,205,187
-65,182,196
-44,127,184
-37,52,148]/256,...
-[100,100,100
-254,178,76
-253,141,60
-240,59,32
-189,0,38]/256};
+    127,205,187
+    65,182,196
+    44,127,184
+    37,52,148]/256,...
+    [100,100,100
+    254,178,76
+    253,141,60
+    240,59,32
+    189,0,38]/256};
 
-figure; 
-hold on;
-thisChLabel='Oz';
+thisChLabel='Cz';
 freqs=TFRhann.freq;
-hp=[];
-for nBl=1:5
-    temp_pow=squeeze(av_logPower(:,nBl,match_str(TFRhann.label,thisChLabel),:));
-    [~,hp(nBl)]=simpleTplot(freqs,temp_pow,0,Colors{1}(nBl,:),0,'-',0.5,1,0,[],2);
+    
+figure;
+for nCond=1:2
+    subplot(1,2,nCond)
+    hold on;
+    hp=[];
+    for nBl=1:5
+        temp_pow=squeeze(av_logPower(CondSubj==Conds{nCond},nBl,match_str(TFRhann.label,thisChLabel),:));
+        [~,hp(nBl)]=simpleTplot(freqs,temp_pow,0,Colors{nCond}(nBl,:),0,'-',0.5,1,0,[],2);
+    end
+    format_fig;
+    xlabel('Freq (Hz)');
+    ylabel('log(Power)');
+    legend(hp,{'B0','B1','B2','B3','B4'});
+    title(sprintf('%s - %s',Conds{nCond},thisChLabel));
+    ylim([-4 6])
 end
-format_fig;
-xlabel('Freq (Hz)');
-ylabel('log(Power)');
-legend(hp,{'B0','B1','B2','B3','B4'});
-title(thisChLabel);
