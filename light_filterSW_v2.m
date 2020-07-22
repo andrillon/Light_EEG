3ad802dd438d16263b1c495b867e65eef1f60256
Thomas Andrillon <thomas.andrillon@gmail.com>
2020-07-22 15:13:00 +1000

    cleaning LS detection

--- light_filterSW_v2.m
+++ light_filterSW_v2.m
@@ -36,7 +36,7 @@ for nS=1:length(List_Subj)
     fprintf('... condition %s\n',CondSubj(nS))
     
     %%% parameters of SW detection
-    paramSW.fixThr=75/2; % if you want to use a fix threshold (eg 50) leave empty ([]) if you want to use the relative
+    paramSW.fixThr=[]; % if you want to use a fix threshold (eg 50) leave empty ([]) if you want to use the relative
     paramSW.prticle_Thr=90; % Choose percentile that you want to select: 80 or 90 or 95
     paramSW.LimFrqW=[1 4]; % Freq range you want to select: [1 4] or [4 10] in Hz
     paramSW.AmpCriterionIdx=4; % Criterion to select waves on: 9 (MaxNegpkAmp) or 11 (MaxPosPeakAmp) or 4 (P2P)
@@ -63,7 +63,10 @@ for nS=1:length(List_Subj)
             thr_Wave=prctile(thisE_Waves(:,paramSW.AmpCriterionIdx),paramSW.prticle_Thr);
         end
         sw_thr=[sw_thr ; [nS 1 nE CondSubj(nS)=='E' thr_Wave]];
-        
+        if thr_Wave>80
+            labels{nE}
+            pause;
+        end
     end
     
     slow_Waves=[];
@@ -78,7 +81,42 @@ for nS=1:length(List_Subj)
         slow_Waves=[slow_Waves ; thisE_Waves(temp_p2p>thr_Wave,:)];
     end
     File_Name2=File_Name(bound{1}(2)+1:end);
-    save([data_path filesep 'SW_fix37uV_' File_Name2(1:end-4)],'slow_Waves','labels','Fs','paramSW');
+    save([data_path filesep 'SW_90P2PbyE_basel_' File_Name2(1:end-4)],'slow_Waves','labels','Fs','paramSW');
     
 end
 
+%%
+cmap=cbrewer('seq','YlOrRd',64); % select a sequential colorscale from yellow to red (64)
+figure; %set(gcf,'Position',[64          33        1097         952]);
+Conds={'D','E'};
+for nC=1:2
+    subplot(1,2,nC);
+    ThresholdSW_All=sw_thr(sw_thr(:, 4) == nC-1,5);
+    ThresholdSW_Group=sw_thr(sw_thr(:, 4) == nC-1,3);
+    ThresholdSW=grpstats(ThresholdSW_All,ThresholdSW_Group,'mean');
+    
+    simpleTopoPlot_ft(ThresholdSW, layout,'labels',[],0,1);
+    title(sprintf('%s',Conds{nC}));
+    colormap(cmap);
+    colorbar;
+    caxis([20 40]);
+    format_fig;
+end
+
+%%
+cmap=cbrewer('seq','YlOrRd',64); % select a sequential colorscale from yellow to red (64)
+figure; %set(gcf,'Position',[64          33        1097         952]);
+Conds={'D','E'};
+for nC=1:2
+    subplot(1,2,nC);
+    ThresholdSW_All=sw_thr(sw_thr(:, 4) == nC-1,5);
+    ThresholdSW_Group=sw_thr(sw_thr(:, 4) == nC-1,3);
+    ThresholdSW=grpstats(ThresholdSW_All,ThresholdSW_Group,'std');
+    
+    simpleTopoPlot_ft(ThresholdSW, layout,'labels',[],0,1);
+    title(sprintf('%s',Conds{nC}));
+    colormap(cmap);
+    colorbar;
+%     caxis([20 40]);
+    format_fig;
+end
